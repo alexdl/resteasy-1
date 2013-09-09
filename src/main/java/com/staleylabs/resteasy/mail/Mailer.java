3ece439d9bda590a0a06890422532273eed9d3a4
Sean Staley <sean.staley@gmail.com>
2013-09-08 20:17:27 -0700

    Added some more logging information.

--- src/main/java/com/staleylabs/resteasy/mail/Mailer.java
+++ src/main/java/com/staleylabs/resteasy/mail/Mailer.java
@@ -1,16 +1,17 @@
 package com.staleylabs.resteasy.mail;
 
 import com.staleylabs.resteasy.beans.EmailMessage;
+import org.apache.log4j.LogMF;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
 
 import javax.annotation.PostConstruct;
 import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
+import javax.mail.internet.*;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -22,6 +23,8 @@ import java.util.Properties;
 
 @Component
 public class Mailer {
+
+    private static final Logger log = Logger.getLogger(Mailer.class);
 
     private static final String SMTP_HOST_NAME = "smtp.sendgrid.net";
 
@@ -49,6 +52,8 @@ public class Mailer {
      * @throws MessagingException
      */
     public boolean sendMessage(EmailMessage message) throws MessagingException {
+        log.info("Request relieved to send a message.");
+
         Session mailSession = Session.getDefaultInstance(properties, smtpAuthenticator);
         Transport transport = mailSession.getTransport();
 
@@ -62,18 +67,38 @@ public class Mailer {
 
         mimeMessage.setContent(multipart);
         mimeMessage.setFrom(new InternetAddress(message.getSender()));
-        mimeMessage.addRecipients(Message.RecipientType.TO, String.valueOf(message.getRecipients()));
+        mimeMessage.addRecipients(Message.RecipientType.TO, generateAddressArray(message.getRecipients()));
 
         if (!CollectionUtils.isEmpty(message.getCcRecipients())) {
-            mimeMessage.addRecipients(Message.RecipientType.CC, String.valueOf(message.getCcRecipients()));
+            mimeMessage.addRecipients(Message.RecipientType.CC, generateAddressArray(message.getCcRecipients()));
         }
 
         mimeMessage.setSubject(message.getSubject());
+
+        log.info("Sending email to " + Arrays.toString(mimeMessage.getAllRecipients()));
 
         transport.connect();
         transport.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
         transport.close();
 
+        log.info("Successfully sent out the requested email. Bye bye!");
+
         return true;
+    }
+
+    protected Address[] generateAddressArray(List<String> addresses) throws AddressException {
+        log.debug("Translating addresses into an array of Address objects.");
+
+        Address[] result = new Address[addresses.size()];
+        int position = 0;
+
+        for (String address : addresses) {
+            result[position] = new InternetAddress(address);
+            position++;
+        }
+
+        LogMF.debug(log, "Translated {0} addresses into Address objects.", position);
+
+        return result;
     }
 }
