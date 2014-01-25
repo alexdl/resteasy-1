a0dd833e9611d6971c7d98ade1339104244ab1a3
Sean Staley <sean.staley@gmail.com>
2014-01-24 23:50:31 -0800

    Fixed some issues with updated dependencies.

--- src/main/java/com/staleylabs/resteasy/api/AdminToolsApiController.java
+++ src/main/java/com/staleylabs/resteasy/api/AdminToolsApiController.java
@@ -1,12 +1,8 @@
 package com.staleylabs.resteasy.api;
 
-import com.staleylabs.resteasy.beans.EmailMessage;
-import com.staleylabs.resteasy.mail.Mailer;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.inject.Inject;
-import javax.mail.MessagingException;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -21,26 +17,13 @@ import java.io.IOException;
 @RequestMapping(value = "/api/admin")
 public class AdminToolsApiController {
 
-    @Inject
-    private Mailer mailman;
-
     @RequestMapping(value = "/mail/test", method = RequestMethod.POST, consumes = "application/json")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void testMailConnection(@RequestBody EmailMessage message, HttpServletResponse response) throws IOException {
-        message.setSubject("RestEasy Test Email");
-        message.setMessageBody("This is a test email from Mr. Admin.");
-        message.setSender("admin@staleylabs.com");
-
-        try {
-            mailman.sendMessage(message);
-        } catch (MessagingException e) {
-            response.sendError(500, "There was an error when trying to send message.");
-        }
+    public void testMailConnection(HttpServletResponse response) throws IOException {
     }
 
     @RequestMapping(value = "/mail/debug/{debugMode}", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void testMailConnection(@PathVariable boolean debugMode) throws IOException {
-        mailman.setDebugMode(debugMode);
     }
 }
